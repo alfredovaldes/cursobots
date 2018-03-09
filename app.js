287157176f90c83af3c8cb19b2ecd4191b6e8662
alfredo valdes <alfredovaldes@uadec.edu.mx>
2018-03-09 00:12:59 -0600

    added index.html

--- app.js
+++ app.js
@@ -19,5 +19,7 @@ server.get(/.*/, restify.plugins.serveStatic({ 'directory': '.', 'default': 'ind
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
+    session.send("You said:"+process.env.MicrosoftAppId+" pass : "+process.env.MicrosoftAppPassword+"   %s", session.message.text);
 });
+console.log(process.env.MicrosoftAppId);
+console.log(process.env.MicrosoftAppPassword);
